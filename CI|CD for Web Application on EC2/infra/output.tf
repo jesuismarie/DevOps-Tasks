a3fb3f995ca8904e61237ebfe97ec3fd5cbb5828
Marie Nazaryan <mari.nazaryan7173@gmail.com>
2026-09-19 20:28:07 +0400

    Make output sensitive

--- CI|CD for Web Application on EC2/infra/output.tf
+++ CI|CD for Web Application on EC2/infra/output.tf
@@ -8,16 +8,19 @@ output "instance_id" {
 output "private_ip" {
   description = "EC2 instance private IP address"
   value       = aws_instance.web-app.private_ip
+  sensitive   = true
 }
 
 # Public IP address of EC2 instance
 output "public_ip" {
   description = "EC2 instance public IP address"
   value       = aws_instance.web-app.public_ip
+  sensitive   = true
 }
 
 # SSH command to connect to the EC2 instance
 output "ssh_command" {
   description = "The SSH command to connect to the EC2 instance"
   value       = "ssh -i ${trimsuffix(var.ssh_public_key_path, ".pub")} ubuntu@${aws_instance.web-app.public_ip}"
+  sensitive   = true
 }
